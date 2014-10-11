6f343514119951d8e3c44aa0a916c753a19f1c40
Fernando Parada <fernandoparada18@yahoo.es>
2014-10-11 00:38:32 -0430

    Se añadio la instancia de la clase Helicoptero

--- instanciacion.py
+++ instanciacion.py
@@ -1,7 +1,15 @@
 from clases import Auto
+from clases import Helicoptero
 
+# instancia de la calse Auto
 mi_auto = Auto(5)
 print mi_auto.gasolina
 mi_auto.arrancar()
 mi_auto.conducir()
 mi_auto.conducir()
+
+# instancia de la clase Helicoptero
+mi_helicoptero = Helicoptero(4)
+print mi_helicoptero.gasolina
+mi_helicoptero.arrancar()
+mi_helicoptero.conducir()
