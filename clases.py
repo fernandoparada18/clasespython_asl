bca7edb6af5f4c188b7309e265b35c5c6d0072dd
Fernando Parada <fernandoparada18@yahoo.es>
2014-10-10 23:58:09 -0430

    Archivo de uso de clase Auto

---
+++ clases.py
@@ -0,0 +1,26 @@
+class Auto:
+    """Abstraccion a objeto de
+    un auto, esta clase va a arrancar
+    un auto, segun su nivel de gasolina"""
+    def __init__(self, gasolina):
+        self.gasolina = gasolina
+        print "Tenemos", gasolina, "litros"
+
+
+    def arrancar(self):
+        if self.gasolina > 0:
+            print "Arranca"
+        else:
+            print "No Arranca"
+
+
+    def conducir(self):
+        if self.gasolina > 0:
+            self.gasolina -= 1
+            print "Quedan", self.gasolina, "litros"
+        else:
+            print "No se mueve..."
+
+mi_auto = Auto(50)
+
+### cuanta gasolina tenemos
