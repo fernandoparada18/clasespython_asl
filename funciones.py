308ab2c3a2da0c57f79c1c2b52129e3206691917
Fernando Parada <fernandoparada18@yahoo.es>
2014-10-10 22:53:35 -0430

    Archivo de muestra de uso de funciones

---
+++ funciones.py
@@ -0,0 +1,12 @@
+# Primero tenemos que importar
+# la funcion print (que viene del modulo )
+# __future__ para poder utilizar a print
+# como una funcion
+
+from __future__ import print_function
+
+def mi_funcion():
+    print("Hola mundo")
+
+def nombres(nombre, apellido, edad):
+    nombre_completo = "
