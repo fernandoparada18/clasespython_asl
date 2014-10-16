2d130cf3c7352589479911438c5a84d3a546cf59
Fernando Parada <fernandoparada18@yahoo.es>
2014-10-16 00:19:31 -0430

    Se añadio la funcion crear_archivo y se modifico las importaciones

--- archivos.py
+++ archivos.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import os
 from __future__ import print_function
+import os
 
 def leer_f(archivo):
     f = open(archivo)
@@ -30,9 +30,25 @@ def rfiles(archivo):
     f = open(archivo)
     print(f.readlines())
 
-
+# Funcion para comprobar si existe un archivo.
 def existe_archivo(archivo):
+    existe = False
     if os.path.exists(archivo):
         print(u"Si es un archivo válido")
+        existe = True
     else:
         print(u"No soy capaz de encontrarlo")
+    return existe
+
+
+def crear_archivo(archivo):
+    if existe_archivo(archivo):
+        print("Y ya existe!!!")
+    else:
+        r = raw_input("Desea crearlo? s/n ")
+        if r == "s" or r == "S":
+            archi = open(archivo,'w')
+            archi.close()
+            print("Archivo creado con exito!!!")
+        else:
+            print("Archivo no creado!!!")
