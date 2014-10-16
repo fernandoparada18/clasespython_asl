afc5ec9338ed605ab8473c9b74197f44cfa8b3bc
Fernando Parada <fernandoparada18@yahoo.es>
2014-10-15 23:47:01 -0430

    Se añadio varias funciones para manejo de archivo

--- archivos.py
+++ archivos.py
@@ -1,15 +1,38 @@
-def rfile(archivo):
+# -*- coding: utf-8 -*-
+import os
+from __future__ import print_function
+
+def leer_f(archivo):
     f = open(archivo)
     for linea in f:
-        print linea
+        print(linea)
     f.close()
+
 
 def wfile(archivo):
     f = open(archivo, 'w')
     f.write("En un lugar de la mancha del cual no me acuerdo su nombre\n")
     f.close()
 
+
 def afile(archivo):
     f = open(archivo, 'a')
     f.write("En un lugar de la mancha del cual no me acuerdo su nombre\n")
     f.close()
+
+
+def rfile(archivo):
+    f = open(archivo)
+    print(f.readline())
+
+
+def rfiles(archivo):
+    f = open(archivo)
+    print(f.readlines())
+
+
+def existe_archivo(archivo):
+    if os.path.exists(archivo):
+        print(u"Si es un archivo válido")
+    else:
+        print(u"No soy capaz de encontrarlo")
